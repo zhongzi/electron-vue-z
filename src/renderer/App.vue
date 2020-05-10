ca0cbeb243068593586123ada8d1826cafd6755d
zhongzi <zhongzigm@gmail.com>
2020-05-10 11:32:07 +0800

    Update 更新依赖基础版本

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1> zhongzi </h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'wechatpy',
+};
+</script>
+
+<style>
+  /* CSS */
+</style>
